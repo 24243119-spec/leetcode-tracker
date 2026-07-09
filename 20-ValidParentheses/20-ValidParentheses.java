08f54d12385b930b2de57d7115555b2a4c85a087
24243119-spec <24243119@drngpit.ac.in>
2026-07-09 10:07:54 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 10:07:52 AM
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else {
+                if (st.isEmpty())
+                    return false;
+
+                char top = st.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{'))
+                    return false;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
