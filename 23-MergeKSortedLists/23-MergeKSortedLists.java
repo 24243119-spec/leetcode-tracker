3bae044ade60e2e072607228084a0e2364b9ff10
24243119-spec <24243119@drngpit.ac.in>
2026-07-20 10:12:52 +0530

    Create 23-MergeKSortedLists

---
+++ 23-MergeKSortedLists/23-MergeKSortedLists.java
@@ -0,0 +1,31 @@
+// Last updated: 7/20/2026, 10:12:51 AM
+import java.util.PriorityQueue;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
+        
+        for (ListNode node : lists) {
+            if (node != null) {
+                minHeap.add(node);
+            }
+        }
+        
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        
+        while (!minHeap.isEmpty()) {
+            ListNode smallest = minHeap.poll();
+            tail.next = smallest;
+            tail = tail.next;
+            
+            if (smallest.next != null) {
+                minHeap.add(smallest.next);
+            }
+        }
+        
+        return dummy.next;
+    }
+}
